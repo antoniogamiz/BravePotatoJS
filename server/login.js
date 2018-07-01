765d93490d48f11ebae348845afa3ba7602efb02
antoniogamiz <antoniogamiz10@gmail.com>
2018-07-01 21:48:44 +0200

    Sistema de loggin hecho

---
+++ server/login.js
@@ -0,0 +1,29 @@
+
+Login = {};
+
+Login.isValidPassword = function(data, callback){
+    Login.db.account.find( {username: data.username, password: data.password} , function(err, res){
+        if(res.length){
+            callback(true);
+        }
+        else{
+            callback(false);
+        }
+    });
+}
+
+Login.isUsernameTaken = function(data, callback){
+    Login.db.account.find( {username: data.username}, function(err, res){
+        console.log(res);
+        if(res.length)
+            callback(true);
+        else
+            callback(false);
+    });
+}
+
+Login.addUser = function(data, callback){
+    Login.db.account.insert( {username: data.username, password: data.password}, function(err){
+        callback(); 
+    });
+}
