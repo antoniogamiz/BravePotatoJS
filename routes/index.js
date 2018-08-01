e3d6b8e5e77f558297babe6ee4c502920b3160e3
Antonio <antoniogamiz10@gmail.com>
2018-08-01 18:22:58 +0200

    popular-manga hecho medienta ejs + update de dependencias

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// TEMPORAL
+
+let populars = new Array(12);
+
+populars.fill( {
+  mangaTitle: "One Piece",
+  coverPath: "/images/one-piece-cover-page.jpg",
+  lastChapter: "Chapter 902",
+  url: "#"
+}, 0, populars.length );
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {mangaCoverPage: populars} );
 });
 
 module.exports = router;
