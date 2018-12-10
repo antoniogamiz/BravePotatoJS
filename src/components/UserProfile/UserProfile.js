77804120be487151c0e0b5987da7473533beab2c
Antonio <antoniogamiz10@gmail.com>
2018-12-10 13:12:17 +0100

    made more responsive and UserProfile__name added

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -5,10 +5,15 @@ class UserProfile extends Component {
   render() {
     const { profileImg } = this.props;
     return (
-        // eslint-disable-next-line
+      <div className="UserProfile">
+        <div className="UserProfile__Name">
+          <p className="UserProfile__Name__p">Anonymous User</p>
+        </div>
+        {/* eslint-disable-next-line */}
         <a href="#">
-            <img className="UserProfile" src={profileImg} alt="user-profile"></img>
+            <img className="UserProfile__Icon" src={profileImg} alt="user-profile"></img>
         </a>
+      </div>
     );   
   }
 }
