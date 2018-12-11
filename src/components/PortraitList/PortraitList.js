5c48747276dbed53cd96f8257871968dd530891e
Antonio <antoniogamiz10@gmail.com>
2018-12-11 09:24:03 +0100

    key bug fixed

--- src/components/PortraitList/PortraitList.js
+++ src/components/PortraitList/PortraitList.js
@@ -5,7 +5,7 @@ import './PortraitList.css';
 export default ({items}) => (
     <div className="PortraitList">
     { items.map( (v,i) => 
-        <div className="PortraitList__Item">
+        <div key={i} className="PortraitList__Item">
             <img className="PortraitList__Item__Img" key={i} src={v} alt="portrait"></img> 
             <p className="PortraitList__Item__Name">One Piece Chapter aaaa</p>
         </div>
