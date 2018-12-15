ff3e2b21bd4b670a83f8144dfc93ef1a41b44435
Antonio <antoniogamiz10@gmail.com>
2018-12-15 13:29:13 +0100

    first approach to fetch data utilities, still working on it

---
+++ src/fetching/homeFetch.js
@@ -0,0 +1,21 @@
+var HTMLParser = require('node-html-parser');
+let request = require("request");
+
+const URL = "https://manganelo.com/home"
+
+let content;
+
+request(
+    { uri: URL },
+    function(error, response, body) {
+        content = body;
+        const root = HTMLParser.parse(content);
+        let latest = root.querySelectorAll('.itemupdate')
+        
+
+    }
+);
+
+ 
+
+
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './Home.css'
 
@@ -12,29 +12,47 @@ import PortraitList from '../components/PortraitList/PortraitList'
 import DetailedPortraitList from '../components/DetailedPortraitList/DetailedPortraitList'
 import Footer from '../components/Footer/Footer'
 
+// import fetchHome from '../fetching/homeFetch' still working on it
 
-export default () => (
-    <div className="home-container">
-        <div className="main-container">
-            <div className="searchbar-container-item">
-                <SearchBar/>
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            popularItems: [],
+            latestItems: []
+        }
+    }
+
+    componentDidMount() {
+        // fetchHome(); still working on it
+    }
+    render() {
+        return (
+            <div className="home-container">
+                <div className="main-container">
+                    <div className="searchbar-container-item">
+                        <SearchBar/>
+                    </div>
+                    <div className="popular-manga-container-item">
+                        <SectionContainer icon={popIcon} title={"Popular Manga"} bg={"rgb(140, 132, 185)"}>
+                        <PortraitList portraits={portraits}/>
+                        </SectionContainer>
+                    </div>
+                    <div className="latest-manga-container-item">        
+                        <SectionContainer icon={latIcon} title={"Latest Manga"} bg={"red"}>
+                        <DetailedPortraitList items={items}/>
+                        </SectionContainer>
+                    </div>
+                    <div className="genres-manga-container-item">            
+                        <SectionContainer icon={genIcon} title={"Genres"} bg={"#16a085"}>          
+                        <GenreList items={genres}/>
+                        </SectionContainer>
+                    </div>
+                </div>
+                <Footer/>
             </div>
-            <div className="popular-manga-container-item">
-                <SectionContainer icon={popIcon} title={"Popular Manga"} bg={"rgb(140, 132, 185)"}>
-                <PortraitList portraits={portraits}/>
-                </SectionContainer>
-            </div>
-            <div className="latest-manga-container-item">        
-                <SectionContainer icon={latIcon} title={"Latest Manga"} bg={"red"}>
-                <DetailedPortraitList items={items}/>
-                </SectionContainer>
-            </div>
-            <div className="genres-manga-container-item">            
-                <SectionContainer icon={genIcon} title={"Genres"} bg={"#16a085"}>          
-                <GenreList items={genres}/>
-                </SectionContainer>
-            </div>
-        </div>
-        <Footer/>
-    </div>
-);
+            );
+        }
+        
+}
+export default Home;
